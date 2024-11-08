62376f3d47c4b1b4323e9d9dc531bb3d630a471a
Laulila <mrslaurapereyra@gmail.com>
2024-11-08 14:45:53 -0300

    Primer commit de mi proyecto

---
+++ App5/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Usuarios)
+admin.site.register(Productos)
+admin.site.register(Ventas_detalles)
+
+# Register your models here.
